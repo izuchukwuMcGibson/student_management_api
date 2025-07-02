1fc9345d65580b7ccc98f0876a7d96232ac9f263
izuchukwuMcGibson <mcgibsononyekachukwu@gmail.com>
2025-07-02 15:14:24 +0100

    Stop tracking src/config and update .gitignore

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-
-connectDB = () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/myDatabase");
-  console.log("Database connected successfully");
-};
-
-module.exports = connectDB;
